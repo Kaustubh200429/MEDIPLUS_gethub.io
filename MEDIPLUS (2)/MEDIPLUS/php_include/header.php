8c3ed7de95e26b1cad05b63a123c0e235b672092
Kaustubh200429 <typxkaustubh@gmail.com>
2024-09-14 15:00:11 +0530

    MEDIPLUS

---
+++ MEDIPLUS (2)/MEDIPLUS/php_include/header.php
@@ -0,0 +1,70 @@
+ <header class="main-header style-one">
+           
+            <div class="header-lower" style="background-color:black;">
+               <div class="auto-container" style="background-color:black;">
+                  <div class="outer-box" style="background-color:black;">
+                     <div class="logo-box" style="background-color:black;">
+                        <div class="pattern" style="background-image: url(assets/images/shape/shape-1.png);"></div>
+                        <div class="bg-color" style="background-color:black;"></div>
+                        <figure class="logo"><a href="index.php"><img src="assets/images/prelogo.png" alt="logo"></a></figure>
+                     </div>
+                     <div class="menu-area">
+                        <!--Mobile Navigation Toggler-->
+                        <div class="mobile-nav-toggler">
+                           <i class="icon-bar"></i>
+                           <i class="icon-bar"></i>
+                           <i class="icon-bar"></i>
+                        </div>
+                        <nav class="main-menu navbar-expand-md navbar-light">
+                           <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent" style="background-color:black;">
+                              <ul class="navigation clearfix">
+                                 <li class="" >
+                                    <a href="index.php" style="color:white;"><font size="3px"><b>Home</b></font></a>
+                                    
+                                 </li>
+                                 <li class="">
+                                    <a href="about_us.php" style="color:white;"><font size="3px"><b>About Us</b></font></a>
+                                   
+                                 </li>
+                                 <li class="">
+                                    <a href="what_we_do.php" style="color:white;"><font size="3px"><b>What We Do?</b></font></a>
+                                    
+                                 </li>
+								 <li class="">
+                                    <a href="appointment.php" style="color:white;"><font size="3px"><b>Appointment</b></font></a>
+                                    
+                                 </li>
+
+                                 <li class="">
+                                    <a href="enquiry.php" style="color:white;"><font size="3px"><b>Register</b></font></a>
+
+                                 </li>
+                                 <li class="">
+                                    <a href="contact.php" style="color:white;"><font size="3px"><b>Contact</b></font></a>
+                                    
+                                 </li>
+                              </ul>
+                           </div>
+                        </nav>
+                     </div>
+                     <div class="btn-box"><a href="login.php" class="theme-btn-one"><i class="icon-image"></i>Login</a></div>
+                  </div>
+               </div>
+            </div>
+            <!--sticky Header-->
+            <div class="sticky-header">
+               <div class="auto-container">
+                  <div class="outer-box">
+                     <div class="logo-box">
+                        <figure class="logo"><a href="index.php"><img src="assets/images/logo.jpg" alt=""></a></figure>
+                     </div>
+                     <div class="menu-area">
+                        <nav class="main-menu clearfix">
+                           <!--Keep This Empty / Menu will come through Javascript-->
+                        </nav>
+                     </div>
+                     <div class="btn-box"><a href="login.php" class="theme-btn-one"><i class="icon-image"></i>Login</a></div>
+                  </div>
+               </div>
+            </div>
+         </header>
